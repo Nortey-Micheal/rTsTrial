2c56b641f4ba4e7d9b00b8a5be14c9fbbad9f5af
Nortey Michael <norteymicheal91@gmail.com>
2025-01-28 21:07:30 +0000

    Enhance Button component styling for improved user experience

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -8,7 +8,7 @@ export function Button() {
 
     return (
         <div>
-        <button className="bg-amber-950" onClick={handleLogIn}>Log In</button>
+        <button className="bg-amber-950 px-10 py-2 text-lg text-amber-100 font-extrabold rounded ml-36 my-16" onClick={handleLogIn}>Log In</button>
         <p>Status {isLoggedIn ? "Logged In" : "Logged Out"}</p>
         </div>
     )
